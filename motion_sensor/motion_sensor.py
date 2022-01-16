2f6653f0fe9638ec13d65092b35943d25a0e22fb
traamcazs <traamcazs@gmail.com>
2022-01-16 11:11:34 +0100

    pir play

--- motion_sensor/motion_sensor.py
+++ motion_sensor/motion_sensor.py
@@ -3,18 +3,15 @@ import time
 import subprocess
 
 pir = MotionSensor(21)
-count = 0
-pir.wait_for_no_motion()
 
 pir.wait_for_no_motion()
 print("Sensor is ready")
 
 def detected():
-    global count
-    print(f"motion detected {count}")
+    subprocess.call("/bin/play")
 
-    count +=1
 while True:
     pir.wait_for_active
     detected()
     subprocess.call("/bin/play")
+    time.sleep(60)
